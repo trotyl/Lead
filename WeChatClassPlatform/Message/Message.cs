5700a8a269877b65d21a0c745d381bfc782f29b6
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-04 19:46:46 +0800

    新建类 TextMessage

---
+++ WeChatClassPlatform/Message/Message.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace WeChatClassPlatform.Message
+{
+    public class TextMessage
+    {
+        public string ToUserName { get; set; }
+        public string FromUserName { get; set; }
+        public int CreateTime { get; set; }
+        public string MsgType { get; set; }
+        public string Content { get; set; }
+        public int MsgId { get; set; }
+
+
+        public TextMessage() { }
+
+        public TextMessage(string xml)
+        {
+            const string format = @"<xml>
+<ToUserName><?<ToUserName>.*?></ToUserName>
+<FromUserName><?<FromUserName>.*?></FromUserName> 
+<CreateTime>?<CreateTime>\d+?</CreateTime>
+<MsgType><?<MsgType>.*?></MsgType>
+<Content><?<Content>.*?></Content>
+<MsgId>?<MsgId>\d+?</MsgId>
+</xml>";
+            Regex re = new Regex(format, RegexOptions.Compiled);
+            Match match = re.Match(xml);
+            if (match.Success)
+            {
+                GroupCollection groups = match.Groups;
+                ToUserName = groups["ToUserName"].Value;
+                FromUserName = groups["FromUserName"].Value;
+                CreateTime = int.Parse(groups["CreateTime"].Value);
+                MsgType = groups["MsgType"].Value;
+                Content = groups["Content"].Value;
+                MsgId = int.Parse(groups["MsgId"].Value);
+            }
+        }
+
+        public string ToXml()
+        {
+            const string format = @"<xml>
+<ToUserName><![CDATA[{0}]]></ToUserName>
+<FromUserName><![CDATA[{1}]]></FromUserName> 
+<CreateTime>{2}</CreateTime>
+<MsgType><![CDATA[{3}]]></MsgType>
+<Content><![CDATA[{4}]]></Content>
+<MsgId>{5}</MsgId>
+</xml>";
+            return string.Format(format, ToUserName, FromUserName, CreateTime, MsgType, Content, MsgId);
+        }
+    }
+}
--- WeChatClassPlatform/Models/User.cs
+++ WeChatClassPlatform/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 
 namespace WeChatClassPlatform.Models
@@ -9,10 +10,58 @@ namespace WeChatClassPlatform.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Number { get; set; }
+
+        private string _number;
+        public string Number
+        {
+            get { return _number; }
+            set
+            {
+                _number = value;
+                TryGetData();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                TryGetData();
+            }
+        }
         public bool IsChatState { get; set; }
-        public string Password { get; set; }
         public List<Exam> ExamList { get; set; }
         public List<Lesson> LessonList { get; set; }
+
+        private void TryGetData()
+        {
+            if (string.IsNullOrEmpty(Number) || string.IsNullOrEmpty(Password))
+            {
+                return;
+            }
+            using (var client = new HttpClient())
+            {
+                var formContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("IDToken0", ""), 
+                    new KeyValuePair<string, string>("IDToken1", Number), 
+                    new KeyValuePair<string, string>("IDToken2", Password), 
+                    new KeyValuePair<string, string>("IDButton", "Submit"), 
+                    new KeyValuePair<string, string>("goto", "aHR0cDovL3BvcnRhbC51ZXN0Yy5lZHUuY24vbG9naW4ucG9ydGFs"), 
+                    new KeyValuePair<string, string>("encoded", "true"), 
+                    new KeyValuePair<string, string>("gx_charset", "UTF-8"), 
+                });
+                var response = client.PostAsync("https://uis.uestc.edu.cn/amserver/UI/Login", formContent).Result;
+                var stringContent = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(stringContent);
+                var allScores =
+                    client.GetStringAsync(
+                        "http://eams.uestc.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action?projectType=MAJOR").Result;
+                Console.WriteLine(allScores);
+            }
+        }
     }
 }
